423eb94ebe9eabb9c08a47d9135ed1bfc8eb1657
Ryan F <ryanfrayser@gmail.com>
2021-07-07 15:30:46 -0600

    setup HTML plugin.

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,7 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
 
-// console.log(checkForName);
-// console.log(handleSubmit)
+console.log(checkForName);
+console.log(handleSubmit)
 
 alert("i exist")
