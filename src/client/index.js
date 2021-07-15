3b969504677f34853d80bf2783f82125214fbaee
Ryan F <ryanfrayser@gmail.com>
2021-07-15 12:58:52 -0600

    slight adjustment in styling of form button.

--- src/client/index.js
+++ src/client/index.js
@@ -11,7 +11,7 @@ import './styles/form.scss'
 import './styles/header.scss'
 
 
-console.log(checkForName);
-console.log(handleSubmit)
+// console.log(checkForName);
+// console.log(handleSubmit)
 
-alert("i exist")
+// alert("i exist")
